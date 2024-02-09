627cc9425d25ef62d6f1bae3a853e21a1b709db6
japerezasd <112252377+japerezasd@users.noreply.github.com>
2024-02-09 16:13:36 +0800

    Add files via upload

--- homePage.js
+++ homePage.js
@@ -4,4 +4,14 @@ function closeOptionTab() {
   document.querySelector('.homePage').classList.remove('openOption');
 
   
+} function clickAnywhere() {
+  document.querySelector('.continueStart').classList.add('continueStartHide')
+ 
+  document.querySelector('.logo').classList.add('continueStartHide')
+  
+
+ document.querySelector('.pageBackground').classList.add('pageBackgroundShow')
+
+ 
 }
+
